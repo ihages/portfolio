28f430f572b9c47f6bf4044aadc7b63704b9e46a
ihages <ihageman23@gmail.com>
2025-08-15 16:07:54 -0400

    fixed routing

--- src/linkmap.tsx
+++ src/linkmap.tsx
@@ -3,13 +3,26 @@ export type Page = {
     url: string,
 }
 
+// Helper to determine base path
+const getBasePath = () => {
+    if (typeof window !== 'undefined') {
+        const host = window.location.host;
+        if (host.startsWith('localhost')) { //handles local
+            return '';
+        } else if (host.includes('github.io')) { //handles prod
+            return '/portfolio';
+        }
+    }
+    return '';
+};
+
 export const LinkMap: Page[] = [
     {
         name: "Home",
-        url: "/portfolio/"
+        url: `${getBasePath()}/`
     },
     {
-        name:"Test Environment",
-        url:"/portfolio/test-environment"
+        name: "Test Environment",
+        url: `${getBasePath()}/test-environment`
     }
-]
+];
