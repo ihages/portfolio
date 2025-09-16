12b83d1039bbc27657efa36820c386c315bfa11b
ihages <ihageman23@gmail.com>
2025-09-16 10:09:59 -0400

    update validslugs

--- src/app/(pages)/test-environment/[...slug]/generateStaticParams.ts
+++ src/app/(pages)/test-environment/[...slug]/generateStaticParams.ts
@@ -1,4 +1,30 @@
 export async function generateStaticParams() {
-  const validSlugs = ["shad-a", "shad-b", "shad-c", "shad-d", "shad-h-m", "shad-n-r", "shad-s", "shad-t"];
+  const validSlugs = [
+    // Comparison pages
+    "comparison-data-display",
+    "comparison-feedback", 
+    "comparison-inputs",
+    "comparison-layouts",
+    "comparison-navigation",
+    "comparison-surfaces",
+    "comparison-utils",
+    // MUI pages  
+    "mui-data-display",
+    "mui-feedback",
+    "mui-inputs", 
+    "mui-layouts",
+    "mui-navigation",
+    "mui-surfaces",
+    "mui-utils",
+    // Shad pages
+    "shad-a", 
+    "shad-b", 
+    "shad-c", 
+    "shad-d", 
+    "shad-h-m", 
+    "shad-n-r", 
+    "shad-s", 
+    "shad-t"
+  ];
   return validSlugs.map((slug) => ({ slug: [slug] }));
 }
