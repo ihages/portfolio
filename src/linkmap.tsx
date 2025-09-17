54f2a3ae85cd49c7d023f5026596a52c098c46e7
ihages <ihageman23@gmail.com>
2025-09-17 10:03:06 -0400

    temporarily removed gallery

--- src/linkmap.tsx
+++ src/linkmap.tsx
@@ -1,27 +1,21 @@
 export type Page = {
     name: string,
     url: string,
-    title: string,
-    metaDesc: string,
 }
 
 export const LinkMap: Page[] = [
     {
         name: "Home",
         url: "/portfolio/",
-        title: "Ihages | Home",
-        metaDesc: "Isabelle Hageman's portfolio and web development testing grounds",
     },
-    {
-        name:"Gallery",
-        url:"/portfolio/gallery",
-        title: "Ihages | Gallery",
-        metaDesc: "Isabelle Hageman's image Gallery",
-    },
+    // {
+    //     name:"Gallery",
+    //     url:"/portfolio/gallery",
+    //     title: "Ihages | Gallery",
+    //     metaDesc: "Isabelle Hageman's image Gallery",
+    // },
     {
         name:"Test Environment",
         url:"/portfolio/test-environment",
-        title: "Ihages | Test Environment",
-        metaDesc: "Isabelle Hageman's testing environment for various npm libraries, currently including ShadCN",
     }
 ]
