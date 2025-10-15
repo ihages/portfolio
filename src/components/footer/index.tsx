3029bd2e5bdabe9f939b157c30e42ea777ba0de1
ihages <ihageman23@gmail.com>
2025-10-15 09:29:43 -0400

    Image http fixes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,11 +3,6 @@ import React from 'react'
 import './style.css'
 import Image from 'next/image'
 
-const IHLogo =
-  process.env.NODE_ENV === 'production'
-    ? 'https://ihages.github.io/portfolio/images/IHlogo.png'
-    : 'http://localhost:3000/portfolio/images/IHlogo.png'
-
 export default function Footer() {
   return (
     <footer className="footer">
@@ -22,7 +17,12 @@ export default function Footer() {
         <p className="!mb-0">Isabelle Hageman 2025</p>
       </div>
       <div className="right">
-        <Image src={IHLogo} width={80} height={80} alt="IH logo" />
+        <Image
+          src="/portfolio/images/IHlogo.png"
+          width={80}
+          height={80}
+          alt="IH logo"
+        />
       </div>
     </footer>
   )
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 import './style.css'
 import Image from 'next/image'
 
-const IHLogo =
-  process.env.NODE_ENV === 'production'
-    ? 'https://ihages.github.io/portfolio/images/IHlogo.png'
-    : 'http://localhost:3000/portfolio/images/IHlogo.png'
-
 export default function Header() {
   return (
     <header className="header">
       <div className="flex flex-row gap-6">
-        <Image src={IHLogo} width={24} height={24} alt="IH logo" />
+        <Image
+          src="/portfolio/images/IHlogo.png"
+          width={24}
+          height={24}
+          alt="IH logo"
+        />
         {LinkMap.map((link) => (
           <a key={link.name} href={link.url}>
             {link.name}
