e8b7bfddb4509eea3713b185731fccd4d32405fe
ihages <ihageman23@gmail.com>
2025-10-15 09:12:58 -0400

    Image http fixes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import './style.css'
 import Image from 'next/image'
 
+const IHLogo =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ihages.github.io/portfolio/images/IHlogo.png'
+    : 'http://localhost:3000/portfolio/images/IHlogo.png'
+
 export default function Footer() {
   return (
     <footer className="footer">
@@ -17,12 +22,7 @@ export default function Footer() {
         <p className="!mb-0">Isabelle Hageman 2025</p>
       </div>
       <div className="right">
-        <Image
-          src="./portfolio/images/IHlogo.png"
-          width={80}
-          height={80}
-          alt="IH logo"
-        />
+        <Image src={IHLogo} width={80} height={80} alt="IH logo" />
       </div>
     </footer>
   )
