42099b99ffff9643a7464c75b2991fe111c74308
ihages <ihageman23@gmail.com>
2025-12-23 12:26:46 -0500

    Copy updates

--- src/app/(pages)/galleries/page.tsx
+++ src/app/(pages)/galleries/page.tsx
@@ -8,11 +8,12 @@ export default function Galleries() {
     <>
       <title>Ihages | Galleries</title>
       <meta name="description" content="" />
+      <Breadcrumbs />
       <div className="page-body galleries">
-        <Breadcrumbs />
-        <h1>H1</h1>
-        <h2>H2</h2>
-        <div className="flex gap-[20px]">div</div>
+        <h1>Art and Tech Galleries</h1>
+        <div className="flex gap-[20px]">
+          <p>Coming Soon!!!</p>
+        </div>
       </div>
     </>
   )
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -9,18 +9,68 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="page-body home">
-      <h1>Hi, I&apos;m Isabelle Hageman</h1>
-      <p>
-        I&apos;m an aspiring full-stack developer, majoring in Computer Science
-        and minoring in Robotics at the University of Cincinnati.
-        <br />
-        <br />I am extrememly passionate in learning and expanding my skillset,
-        all the way from circuitry to graphic design.
-        <br />
-        <br />
-        I&apos;m a proud alum of GirlsWhoCode and the INTERalliance of Greater
-        Cincinnati.
-      </p>
+      <h1 className="pt-[75px] underline decoration-dashed decoration-primary">
+        Hi, I&apos;m Isabelle Hageman
+      </h1>
+      <h2>About Me</h2>
+      <ul>
+        <li>
+          I am a second year at the University of Cincinnati studying for my
+          Bachelor&apos;s in Computer Science and Minor in Robotics Engineering
+        </li>
+        <li>
+          I have an expansive background in the arts, including working for
+          ArtWorks Cincinnati as a muralist, face painter contractor with the
+          Cincinnati Circus, and having multiple pieces in art shows throughout
+          grade school and high school
+        </li>
+        <li>
+          I have passions in creative writing, although I havenr&apos;t
+          published any content. I am dabbling with the idea of starting a blog
+        </li>
+      </ul>
+      <h2>Featured In</h2>
+      <ul>
+        <li>
+          <a
+            href="https://www.uc.edu/news/articles/2025/08/co-op-broadens-horizons.html"
+            target="_blank"
+          >
+            Co-op Broadens Horizons
+          </a>{' '}
+          by{' '}
+          <a
+            href="https://www.uc.edu/news/search.html?authors=glassei&force-search=true"
+            target="_blank"
+          >
+            Emily Glass
+          </a>
+        </li>
+      </ul>
+      <h2>Contact</h2>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/isabelle-hageman/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/ihages" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://uc.joinhandshake.com/profiles/ihageman"
+            target="_blank"
+          >
+            Handshake
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
